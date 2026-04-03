3bd33eee90ef244db4ad3d0b6d640e6072227571
Erikas Laptevas <elaptevas@litit.tech>
2026-04-03 15:40:51 +0300

    SequenceModule sequenceConsmer to printer bind fix

--- calculator-base/src/main/java/org/example/modules/SequenceModule.java
+++ calculator-base/src/main/java/org/example/modules/SequenceModule.java
@@ -3,6 +3,7 @@ package org.example.modules;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
 import org.example.consumers.SequencePrinter;
+import org.example.interfaces.SequenceConsumer;
 import org.example.predicates.EvenPredicate;
 
 import java.util.List;
@@ -13,7 +14,7 @@ public class SequenceModule extends AbstractModule {
 
     @Override
     protected void configure() {
-        bind(new TypeLiteral<BiConsumer<List<Long>, Predicate<Long>>>() {}).to(SequencePrinter.class);
+        bind(new TypeLiteral<SequenceConsumer>() {}).to(SequencePrinter.class);
         bind(new TypeLiteral<Predicate<Long>>() {}).to(EvenPredicate.class);
     }
 }

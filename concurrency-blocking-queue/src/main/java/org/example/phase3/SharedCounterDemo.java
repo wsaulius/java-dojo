f77dc5f090c49238e104b4bc1011a41fd008c8c9
Arturas97 <akazlauskas@litit.tech>
2026-05-21 08:13:10 +0000

    Phase 3 — Concurrency Fundamentals Example: Shared counter with race condition. Added simple counter and a demo with multiple threads incrementing the same value, exposing the problems with concurrency without any preventatives.

---
+++ concurrency-blocking-queue/src/main/java/org/example/phase3/SharedCounterDemo.java
@@ -0,0 +1,29 @@
+package org.example.phase3;
+
+
+public class SharedCounterDemo {
+    public static void main(String[] args) throws InterruptedException {
+        SimpleCounter counter = new SimpleCounter();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Expected value 20000. \nFinal counter value: " + counter.count);
+
+    }
+}
---
+++ concurrency-blocking-queue/src/main/java/org/example/phase3/SimpleCounter.java
@@ -0,0 +1,9 @@
+package org.example.phase3;
+
+public class SimpleCounter {
+     int count = 0;
+
+     void increment() {
+         count++;
+     }
+}
